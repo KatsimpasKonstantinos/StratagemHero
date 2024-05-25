5b7e1380a7f779ddfa37eaed0957e45ee3a2ce90
Konsti <134274002+KatsimpasKonstantinos@users.noreply.github.com>
2024-05-25 20:04:32 +0200

    jawoll

---
+++ website/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Game from './Game/Game.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Game />
+      </header>
+    </div>
+  );
+}
+
+export default App;
