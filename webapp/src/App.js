2524b117258cf8b2ad64cb53337bc3374dc1af84
Miku <134274002+KatsimpasKonstantinos@users.noreply.github.com>
2024-06-03 19:23:07 +0200

    cleanup

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="whiteLine top"></div>
+      <div className="whiteLine bottom"></div>
     </div>
   );
 }
---
+++ webapp/src/components/whiteLine.js
@@ -0,0 +1,10 @@
+import './whiteLine.css';
+
+function whiteLine() {
+  return (
+    <div className="whiteLine">
+    </div>
+  );
+}
+
+export default whiteLine;
