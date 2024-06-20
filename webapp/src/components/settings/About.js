0225d4e6adcf6b5b56e5fd4dbf237ab26491a9b5
Miku <134274002+KatsimpasKonstantinos@users.noreply.github.com>
2024-06-20 23:01:31 +0200

    different cursor

--- webapp/src/components/settings/About.js
+++ webapp/src/components/settings/About.js
@@ -7,7 +7,7 @@ function About() {
                 <p className="AboutTitle">StratagemHero Webapp</p>
                 <p className="AboutText">Build by <a href="https://konstantinos.katsimpas.com/">Konstantinos Katsimpas</a></p>
                 <p className="AboutText">Give this Project a Star on <a href="https://github.com/KatsimpasKonstantinos/StratagemHero">Github</a></p>
-                <p className="AboutText">Support me on <a href="https://ko-fi.com/konstantinoskatsimpas">Ko-fi</a></p>
+                <p className="AboutText">Buy me a Liber-tea on <a href="https://ko-fi.com/konstantinoskatsimpas">Ko-fi</a></p>
             </div>
         </div>
     );
