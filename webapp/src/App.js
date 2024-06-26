247e3d35e16755da989b1ac24a20d04c4e9cd33b
Konsti <134274002+KatsimpasKonstantinos@users.noreply.github.com>
2024-06-26 20:19:14 +0200

    check browser version

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -5,6 +5,7 @@ import StartView from './views/StartView.js';
 import InvalidScreen from './components/InvalidScreen.js';
 import { useEffect } from 'react';
 import SettingsView from './views/SettingsView.js';
+import BrowserVersion from './components/BrowserVersion.js';
 
 const keyMap = {
   "ArrowUp": "up",
@@ -89,30 +90,12 @@ function App(props) {
     }
   });
 
-  navigator.sayswho = (function () {
-    var ua = navigator.userAgent;
-    var tem;
-    var M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
-    if (/trident/i.test(M[1])) {
-      tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
-      return 'IE ' + (tem[1] || '');
-    }
-    if (M[1] === 'Chrome') {
-      tem = ua.match(/\b(OPR|Edge)\/(\d+)/);
-      if (tem != null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
-    }
-    M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
-    if ((tem = ua.match(/version\/(\d+)/i)) != null) M.splice(1, 1, tem[1]);
-    return M.join(' ');
-  })();
-
-  console.log(navigator.sayswho); // outputs: `Chrome 62`
-
 
   let screen = computed(() => {
     switch (mainScreenString.value.split(" ")[0]) {
       case "start":
-        return <StartView mainScreenString={mainScreenString} keyPressed={keyPressed} />;
+        //return <StartView mainScreenString={mainScreenString} keyPressed={keyPressed} />;
+        return <BrowserVersion />;
       case "game":
         return <GameView mainScreenString={mainScreenString} keyPressed={keyPressed} stratagemsData={stratagemsData} />;
       case "settings":
